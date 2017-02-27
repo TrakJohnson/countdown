cb7d898d77e9c7066174ee31db9effe6a3b5fc4c
Trak Johnson <trakjohnsonweb@gmail.com>
2017-02-27 21:37:30 +0100

    styling and layouts done + installed jQuery plugin

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// the example from the docs :)
+$('#clock').countdown("2020/10/10", function(event) {
+  var totalHours = event.offset.totalDays * 24 + event.offset.hours;
+  $(this).html(event.strftime(totalHours + ' hr %M min %S sec'));
+});
 
