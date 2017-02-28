66471c598c6b1d6a46e498c16475f369697c352d
Jingjie Yang <j.yang19@ejm.org>
2017-02-28 10:29:14 +0100

    Unleashed the beast
    made the countdown work by adding js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // the example from the docs :)
-$('#clock').countdown("2020/10/10", function(event) {
-  var totalHours = event.offset.totalDays * 24 + event.offset.hours;
-  $(this).html(event.strftime(totalHours + ' hr %M min %S sec'));
+$(document).ready(function() {
+    $('#timer_container').countdown("2017/03/22", function(event) {
+        $('#days_in').text(event.strftime('%D'));   
+        $('#hours_in').text(event.strftime('%H'));
+        $('#minutes_in').text(event.strftime('%M'));
+        $('#seconds_in').text(event.strftime('%S'));
+    });
 });
 
