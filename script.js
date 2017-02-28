c95efa01f693cbfc49331a3a655940c40295bd66
Jingjie Yang <j.yang19@ejm.org>
2017-02-28 19:05:45 +0100

    Théo au boulot!
    - added display seconds only option (html implementation to be done by
    trak)
    - corrected the hour
    - implemented countdown by url

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// the example from the docs :)
 $(document).ready(function() {
-    $('#timer_container').countdown("2017/03/22", function(event) {
+    var date = getURLParameter("date");
+    var date = date.split(":")[0].split("-").join("/") + " " + date.split(":")[1].split("-").join(":");
+    alert(date);
+    
+    $('#timer_container').countdown(date, function(event) {
         $('#days_in').text(event.strftime('%D'));   
         $('#hours_in').text(event.strftime('%H'));
         $('#minutes_in').text(event.strftime('%M'));
@@ -8,3 +11,26 @@ $(document).ready(function() {
     });
 });
 
+function getURLParameter(name) {
+    return decodeURIComponent(
+        (new RegExp('[?|$]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(
+            location.search
+        ) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
+
+/*
+// only in seconds
+$(document).ready(function() {
+    $('#timer_container').countdown("2017/03/22", function(event) {
+        //$('#days_in').text(event.strftime('%D'));   
+        //$('#hours_in').text(event.strftime('%H'));
+        //$('#minutes_in').text(event.strftime('%M'));
+        
+        var days = event.offset.totalDays;
+        var hrs = event.offset.hours + days * 24;
+        var minutes = event.offset.minutes + hrs * 60;
+        var seconds = event.offset.seconds + minutes * 60;
+        $('#seconds_in').text(seconds);
+    });
+});
+*/
